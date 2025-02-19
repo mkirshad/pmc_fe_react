d09859952dbf7f8003dd87c83832fcc4ef07f695
Muhammad Kashif Irshad <kashif.ir@gmail.com>
2025-02-19 15:06:57 +0500

    commit
    commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import './index.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <App />
-    </React.StrictMode>,
+    // </React.StrictMode>,
 )
 
 // Register the service worker
