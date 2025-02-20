dbba90dc57492c223e30dd3fc23c5860fc5a95d6
Muhammad Kashif Irshad <kashif.ir@gmail.com>
2025-02-20 14:44:50 +0500

    commit
    commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-globals */
 
 // Cache version
-const CACHE_NAME = "pwa-cache-v47"; // Increment version to force cache update
+const CACHE_NAME = "pwa-cache-v48"; // Increment version to force cache update
 const STORE_NAME = "offline-requests";
 const DB_NAME = "OfflineDB";
 const API_CACHE_NAME = "api-cache";
