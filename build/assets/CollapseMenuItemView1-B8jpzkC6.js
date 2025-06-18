8acc51a07c6ae01b45754a02066525640d0dc751
Muhammad Kashif Irshad <kashif.ir@gmail.com>
2025-06-18 12:16:07 +0500

    inspection changes
    inspection changes

---
+++ build/assets/CollapseMenuItemView1-B8jpzkC6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D8nJZtJt.js";const s=()=>e.jsx("div",{children:"CollapseMenuItemView1"});export{s as default};
---
+++ build/assets/SingleMenuView-Ba0v2m05.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D8nJZtJt.js";const n=()=>e.jsx("div",{children:"Single Menu View"});export{n as default};
